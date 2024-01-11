673c997a3b01f4b1b7c99ab057c6f76e5c27bddc
ziord <jeremiahikosin@gmail.com>
2024-01-11 14:20:46 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! command -v node &> /dev/null || ! command -v npm &> /dev/null; then
+    if [ "$(uname)" == "Linux" ]; then
+        curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash -
+        sudo apt-get install -y nodejs
+    elif [ "$(uname)" == "Darwin" ]; then
+        brew install node
+    else
+        echo "Unsupported operating system: $(uname)"
+        exit 1
+    fi
+else
+    echo "Node.js and npm are already installed."
+fi
+
+npm i -g gulp-cli
+make
